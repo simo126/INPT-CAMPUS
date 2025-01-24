b59d8dbefad135272227fea8e4f418c5ae799240
simo126 <simo82220@gmail.com>
2025-01-24 21:39:04 +0100

    Add React frontend project

---
+++ inpt-campus-frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>Sbetios & Kssiminho</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint aliquam
+        praesentium, ratione, eligendi repellat eum dolores nostrum alias
+        repellendus provident repudiandae. Impedit asperiores modi adipisci.
+        Iure ab rerum id sunt?
+      </p>
+    </>
+  );
+}
+
+export default App;
